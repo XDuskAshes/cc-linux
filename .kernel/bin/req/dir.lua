8df89dbc08b30964eb2db1d0bb4b7cc35545d8c9
Dusk C <thegaymerdummy@gmail.com>
2023-05-05 20:59:45 -0400

    A bunch of changes.
    1. Added folders to '.kernel/bin/':
        opt
        req
    
    opt - optional binaries
    req - required, will be checked
    
    2. Moved the drive, modem, and monitor bins to '.kernel/bin/opt/'
    3. Created '.kernel/bin/opt/dir.lua'
    4. Removed placeholders for the folders mentioned above.

---
+++ .kernel/bin/req/dir.lua
@@ -0,0 +1,17 @@
+function tmp(a)
+    if a == "make" then
+        fs.makeDir("tmp/")
+    end
+
+    if a == "clear" then
+        if fs.exists("tmp/") == true then
+            shell.run("rm tmp/*")
+        end
+    end
+
+    if a == "delete" then
+        if fs.exists("tmp/") == true then
+            fs.delete("tmp/")
+        end
+    end
+end
