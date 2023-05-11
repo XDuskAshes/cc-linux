29cc893eae34c39544133d38f7e28d732238e823
Dusk C <thegaymerdummy@gmail.com>
2023-05-10 23:58:54 -0400

    Changing the name.

--- kernel.lua
+++ kernel.lua
@@ -2,14 +2,7 @@
 
 --[[
 
-+============================================+
-|  ____   _____  _ _ _  _____  _____  __ __  |
-| |    \ |  _  || | | ||   | ||     ||  |  | |
-| |  |  ||     || | | || | | ||-   -||-   -| |
-| |____/ |__|__||_____||_|___||_____||__|__| |
-|               KERNEL v1.0.0                |
-+============================================+
-
+CC-LINUX Kernel
 Version: 1.0.0
 Developed from: 4/18/2023 - [DATE]
 Developer: Dusk (Dusk#0834/XDuskAshesReal)
@@ -21,7 +14,7 @@ Total lines of code: (when it's done, goes here.)
 
 --[[
     I am going to entirely re-code this, but I'll explain it's purpose in this blurb.
-    The point of this file will to be the handler for all processes of DAWNIX, as well as be an actual kernel.
+    The point of this file will to be the handler for all processes of CC-Linux, as well as be an actual kernel.
     This will be the point where all the things are called in functions, so that one can simply call from kernel.
     All contribution to brainstorming and code changes in this file are welcome in here. Just don't touch this blurb. Add your own darn blurbs!
     -Dusk, 5/9/2023 (6:20 pm)
