e6d12985fbaedb09ca6ce0f03692abb2b0ccc8a7
Dusk C <thegaymerdummy@gmail.com>
2023-05-16 08:38:10 -0400

    Make 4 functions
    1. checkrootfs() - check the root filesystem of CC-Linux
    2. getContent(dir) - get contents from a directory and put it in a
    table
    3. displayContent() - displays contents gotten from 'getContent()'
    4. purgeContents() - purge the contents gotten from 'getContent()'

--- kernel.lua
+++ kernel.lua
@@ -20,8 +20,12 @@ Total lines of code: (when it's done, goes here.)
     -Dusk, 5/9/2023 (6:20 pm)
 ]]
 
---Screen message handler/displayer
-function scrMSG(process,type,msg)
+local expect = require "cc.expect"
+
+function scrMSG(process,type,msg) --Screen message handler/displayer.
+    expect(1, process, "string")
+    expect(2, type, "number")
+    expect(3, msg, "string")
     --Type is a number. 1 = Good (Green). 2 = Warning/Could be bad (Yellow), and 3 = Error (Red, obviously.)
     if type == 1 then
         term.setTextColor(colors.green)
@@ -31,6 +35,68 @@ function scrMSG(process,type,msg)
         print("(WARN)","[",process,"]",msg)
     elseif type == 3 then
         printError("(ERROR)","[",process,"]",msg)
+    elseif error() then
+        printError("[ kernel:scrMSG ] encountered an error:",error)
     end
     term.setTextColor(colors.white)
+end
+
+function halt(haltthis,dothis) --Stop a function short, run a different one.
+    expect(1, haltthis, "function")
+    expect(1, dothis, "function")
+    error(haltthis)
+    dothis()
+    --this wont work at all will it
+end
+
+function checkrootfs() --check the root filesystem of CC-Linux to ensure it exists, and error if something doesn't.
+    local root = { --root, stole from boot/dircheck.lua
+    "boot",
+    "conf",
+    "dev",
+    "exec",
+    "home",
+    "lib",
+    "sys",
+    "temp",
+    "usr"
+}
+
+    for k,v in pairs(root) do
+        if fs.exists(v) then
+            scrMSG("kernel:checkrootfs",1,"checked dir exists") --yes I use my own functions in the kernel, hush
+        else
+            scrMSG("kernel:checkrootfs",3,"checked dir does not exist")
+            printError("Create directory:",v,"and it's contents")
+            error("Please create root directory listed above.")
+        end
+    end
+end
+
+dircontent = {}
+
+function getContent(dir) --Get contents from a directory and put it in a table.
+    expect(1,dir,"string")
+    local fullPath = shell.resolve(dir)
+    
+    if not fs.exists(fullPath) or not fs.isDir(fullPath) then
+        error("Directory does not exist: " .. dir)
+    end
+
+    for _, item in ipairs(fs.list(fullPath)) do
+        local path = fs.combine(dir, item)
+        table.insert(dircontent, path)
+    end
+end
+
+function displayContent() --Displays contents gotten from 'getContent()'
+    for k,v in pairs(dircontent) do
+        print(k,"|",v)
+    end
+end
+
+function purgeContents() --Purge the contents gotten from 'getContent()'
+    for k in pairs(dircontent) do
+        dircontent[k] = nil
+    end
 end
